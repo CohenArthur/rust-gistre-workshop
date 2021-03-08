46c4eb6f2caae2ffa18c05ac619973a757db8ed7
CohenArthur <arthur.cohen@epita.fr>
2021-03-09 00:00:24 +0100

    yes: Add simplest version

--- 0_intro/live_coding/yes/src/main.rs
+++ 0_intro/live_coding/yes/src/main.rs
@@ -48,7 +48,37 @@
 //! Using the clap crate, parse arguments given to your program and display something useful
 //! when using the `-h|--help` option. The program should contain an author, a version
 //! and a small description!
+//!
+//! ## Going further - Unit testing stdout
+
+pub fn get_string() -> String {
+    // Show that we can return it either using the return keyword or as an expression
+    String::from("y")
+}
+
+// Either pass the value by reference or by moving it but note how it's different
+pub fn show_string(s: &String) {
+    println!("{}", s);
+}
 
 fn main() {
-    println!("Hello, world!");
+    // Ask if the variable needs to be mutable or not
+    let s = get_string();
+
+    // Make sure to show that we can use `loop` instead of `while true`
+    loop {
+        show_string(&s);
+    }
+}
+
+// Explain conditional compilation
+#[cfg(test)]
+mod tests { // Quickly explain modules
+    use super::*; // Explain `use` and refer to C++ namespaces + the asterisk
+
+    // Refer to Java Attributes, like @JUnitTest that we did
+    #[test]
+    fn get_string_basic() {
+        assert_eq!(get_string(), "y"); // Talk about assert_eq! and assert!
+    }
 }
