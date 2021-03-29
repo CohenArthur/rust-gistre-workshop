ec45054b99df0b2f9a72f980f68d3892cab4e5c8
Pierrick Guillaume <pierrick.guillaume@epita.fr>
2021-03-29 21:56:38 +0200

    PATCH: no more use String to split into words for acrony

--- 1_slices_modules_and_objects/exercises/acronym/src/lib.rs
+++ 1_slices_modules_and_objects/exercises/acronym/src/lib.rs
@@ -10,7 +10,7 @@
 /// ```
 // Side note: this doc example is also tested when running `cargo test`
 pub fn make_acronym(name: &str) -> String {
-    String::from(name).split_whitespace().map(|word| word.chars().nth(0).unwrap().to_ascii_uppercase()).collect::<String>()
+    name.split_whitespace().map(|word| word.chars().nth(0).unwrap().to_ascii_uppercase()).collect::<String>()
 }
 
 #[cfg(test)]
